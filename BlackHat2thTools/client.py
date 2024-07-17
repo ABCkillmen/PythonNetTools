f8dba2a9af3ae293b06d9df35fe28befc228d433
ABCkillmen <liujiaqi6677@126.com>
2024-07-17 16:59:27 +0800

    add server.py and modify main to client

--- BlackHat2thTools/client.py
+++ BlackHat2thTools/client.py
@@ -1,13 +1,11 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# !/usr/bin/python3
 import socket
 
 def tcp_client():
-    target_host = "127.0.0.1"
-    target_port = 80
+    target_host = "0.0.0.0"
+    target_port = 9998
 
     # create a socket object
     # AF_INET参数表示我们将使用标准的IPv4地址或主机名，SOCK_STREAM表示这是一个TCP客户端。
@@ -17,10 +15,10 @@ def tcp_client():
     client.connect((target_host, target_port))
     # send some data
     # 并发送一些bytes类型的数据。
-    client.send(b"GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
+    client.send(b"ABCDEF")
     # receive some data
     # 最后一步，接收返回的数据并将其打印到屏幕上。
-    response = client.recv(4096)
+    # response = client.recv(4096)
 
     print(response.decode())
     client.close()
@@ -49,7 +47,30 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    # tcp_client()
-    udp_client()
+    tcp_client()
+    # udp_client()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BlackHat2thTools/server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import socket
+import threading
+
+IP = '0.0.0.0'
+PORT = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 先指定服务器应该监听哪个IP地址和端口。
+    server.bind((IP, PORT))
+    # 让服务器开始监听，并把最大连接数设为5。
+    server.listen(5)
+    print(f'[*] Listening on {IP}:{PORT}')
+    # 让服务器进入主循环中，并在该循环中等待外来连接。
+    while True:
+        # 当一个客户端成功建立连接的时候，
+        # 将接收到的客户端socket对象保存到client变量中，
+        # 将远程连接的详细信息保存到address变量中。
+        client, address = server.accept()
+
+        print(f'[*] Accepted connection form {address[0]}:{address[1]}')
+        # 创建一个新的线程，让它指向handle_client函数，并传入client变量。
+        client_handler =threading.Thread(target=handle_client(), args=(client,))
+        # 创建好后，我们启动这个线程来处理刚才收到的连接。
+        client_handler.start()
+        # 与此同时服务端的主循环也已经准备好处理下一个外来连接。
+
+# handle_client函数会调用recv()接收数据，并给客户端发送一段简单的回复。
+def handle_client(client_socket):
+    with client_socket as sock:
+        requests = sock.recv(1024)
+        print(f'[*] Received: {requests.decode("utf-8")}')
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
